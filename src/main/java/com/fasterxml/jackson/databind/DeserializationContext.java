ba38859b561f071ebb36354c986dc713e5bf8595
Aaron Belenky <aaron.b@sealsecurity.io>
2024-12-09 17:16:23 +0200

    CVE-2022-42003

--- src/main/java/com/fasterxml/jackson/databind/DeserializationContext.java
+++ src/main/java/com/fasterxml/jackson/databind/DeserializationContext.java
@@ -94,6 +94,7 @@ public abstract class DeserializationContext
      */
     protected final int _featureFlags;
 
+    protected int _primitiveRecursionDepth;
     /**
      * Currently active view, if any.
      */
@@ -162,6 +163,7 @@ public abstract class DeserializationContext
         }
         _cache = cache;
         _featureFlags = 0;
+        _primitiveRecursionDepth = 0;
         _config = null;
         _injectableValues = null;
         _view = null;
@@ -176,6 +178,7 @@ public abstract class DeserializationContext
         
         _config = src._config;
         _featureFlags = src._featureFlags;
+        _primitiveRecursionDepth = 0;
         _view = src._view;
         _parser = src._parser;
         _injectableValues = src._injectableValues;
@@ -191,9 +194,10 @@ public abstract class DeserializationContext
     {
         _cache = src._cache;
         _factory = src._factory;
-        
+
         _config = config;
         _featureFlags = config.getDeserializationFeatures();
+         _primitiveRecursionDepth = 0;
         _view = config.getActiveView();
         _parser = p;
         _injectableValues = injectableValues;
@@ -209,6 +213,7 @@ public abstract class DeserializationContext
 
         _config = src._config;
         _featureFlags = src._featureFlags;
+        _primitiveRecursionDepth = 0;
         _view = src._view;
         _injectableValues = null;
     }
@@ -359,6 +364,18 @@ public abstract class DeserializationContext
      */
     public final boolean hasSomeOfFeatures(int featureMask) {
         return (_featureFlags & featureMask) != 0;
+    }
+
+    public final int getPrimitiveRecursionDepth(){
+        return _primitiveRecursionDepth;
+    }
+
+    public final void incPrimitiveRecursionDepth() {
+        _primitiveRecursionDepth += 1;
+    }
+
+    public final void resetPrimitiveRecursionDepth() {
+        _primitiveRecursionDepth = 0;
     }
     
     /**
